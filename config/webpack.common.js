efc3fa12b22ade1de41520b0f525e92b3aeb1f20
Aaron Williamson <guitarfanman@gmail.com>
2019-03-14 13:24:16 -0600

    Initial app layout

---
+++ config/webpack.common.js
@@ -0,0 +1,34 @@
+// Turn off commonjs linting for this file
+/* eslint import/no-commonjs: "off" */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
---
+++ config/webpack.prod.js
@@ -0,0 +1,8 @@
+// Turn off commonjs linting for this file
+/* eslint import/no-commonjs: "off" */
+const merge = require('webpack-merge'); // eslint-disable-line import/no-extraneous-dependencies
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+});
