d34c9580cc1a1d44bb60c51a9b12035c05928709
shivanithakkar <36409213+shivanithakkar@users.noreply.github.com>
2019-04-08 21:14:11 -0600

    Downloaded updated master files

---
+++ config/webpack.dev.js
@@ -0,0 +1,29 @@
+/* eslint import/no-commonjs: "off", import/no-extraneous-dependencies: "off" */
+const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+});
---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return <div>This is the Squeegee apperidoo!</div>;
+}
+
+export default App;
