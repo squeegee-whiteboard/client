b79982d13e778b90f148b2aebea6117735fe99b4
Joel <joel.vanegmond@ucalgary.ca>
2019-04-13 15:10:08 -0600

    Fixed config linting

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,6 +1,6 @@
 // Values used to contact the API server
 export default {
-    URL_SCHEME: process.env.SCHEME || 'http',
-    IP: process.env.API_IP || 'localhost',
-    PORT: process.env.API_PORT || '3000',
-  };
+  URL_SCHEME: process.env.SCHEME || 'http',
+  IP: process.env.API_IP || 'localhost',
+  PORT: process.env.API_PORT || '3000',
+};
