4c786670e03b31b3b8468d9a9a628ec1e873dc9d
shivanithakkar <36409213+shivanithakkar@users.noreply.github.com>
2019-04-13 12:52:52 -0600

    Temp commit for backup

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -5,8 +5,12 @@ import {
   Route,
   // Redirect,
 } from 'react-router-dom';
-// import Whiteboard from './whiteboard';
+import Whiteboard from './whiteboard';
 import Dashboard from './dashboard';
+
+import Login from './login';
+import Signup from './signup';
+import './login-signup.css';
 
 import Nav from '../components/nav';
 
@@ -29,7 +33,9 @@ function App() {
         <Nav />
       </nav>
       <Switch>
-        {/* <Route exact path="/" component={DashboardRedirect} /> */}
+        {/* <Route exact path="/" component={DashboardRedirect} />  */}
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/whiteboard/:id" component={Whiteboard} />
       </Switch>
