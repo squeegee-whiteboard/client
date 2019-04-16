13e0d5e554c012c50d93d1ebb5fccadf8619b73a
Aaron Williamson <guitarfanman@gmail.com>
2019-04-15 21:26:30 -0600

    Change webpack devtool for development mode

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,7 +5,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'eval-source-map',
   devServer: {
     contentBase: './dist',
   },
